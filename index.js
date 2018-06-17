1c75c55d4d94542212a38b8cc3cd4bed05b2c59f
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-06-17 01:22:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested div div div div')
+}
+
+function increaseRankBy(n) {
+  const rank = 
+  document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + parseInt(n)
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
